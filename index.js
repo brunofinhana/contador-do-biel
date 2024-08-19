4f5a4ed5d65e328b65c65de2e5a16b85fdbcfe09
Bruno Finhana <42972701+brunofinhana@users.noreply.github.com>
2024-08-19 17:58:49 -0300

    Arquivos básicos do site

---
+++ index.js
@@ -0,0 +1,79 @@
+let botaoMaisCasa = document.getElementById("botao-mais-casa")
+let botaoMenosCasa = document.getElementById("botao-menos-casa")
+let placarCasa = document.getElementById("placar-casa")
+
+let botaoMaisVisitante = document.getElementById("botao-mais-visitante")
+let botaoMenosVisitante = document.getElementById("botao-menos-visitante")
+let placarVisitante = document.getElementById("placar-visitante")
+
+let counterCasa = 0
+let counterVisitante = 0
+
+let botaoSalvar = document.getElementById("botao-salvar")
+let placaresSalvosElemento = document.getElementById("placares-salvos-elemento")
+
+// Funções Casa
+function maisCasa() {
+    counterCasa += 1
+    placarCasa.textContent = counterCasa
+}
+
+function menosCasa() {
+    if(counterCasa <= 0)
+        counterCasa = 0
+    else
+        counterCasa -= 1
+        placarCasa.textContent = counterCasa
+}
+
+// Funções Visitante
+function maisVisitante() {
+    counterVisitante += 1
+    placarVisitante.textContent = counterVisitante
+}
+
+function menosVisitante() {
+    if(counterVisitante <= 0)
+        counterVisitante = 0
+    else
+        counterVisitante -= 1
+        placarVisitante.textContent = counterVisitante
+}
+
+// Destacando o vencedor
+if(counterCasa > counterVisitante) {
+    placarCasa.style.color = "lime"
+    console.log(placarCasa)
+} else if(counterVisitante > counterCasa) {
+    placarVisitante.style.color = "lime"
+}
+
+// Função Reiniciar Placar
+function reiniciarPlacar() {   
+    counterCasa = 0
+    counterVisitante = 0
+
+    placarCasa.textContent = 0
+    placarVisitante.textContent = 0
+}
+
+// Função Salvar Placar
+function salvarPlacar() {
+    const diaMesAno = new Date()
+    let diaPartida = "(" + diaMesAno.getDate() + "/" + diaMesAno.getMonth() + ")"
+    
+    // Definição do conteúdo salvo
+    // INCREMENTAR:
+        // Identificar como "casa" e "visitante" cada placar
+        // Exibir a data de salvamento
+    let placarSalvo =
+        counterCasa + " x " + counterVisitante + "\n" +
+        diaPartida +
+        " | "
+
+    // Exibição incremental dos conteúdos definidos salvos (acima)
+        // Exibir em um elemento de html específico (<p></p>?)
+    placaresSalvosElemento.textContent += placarSalvo
+
+    reiniciarPlacar()
+}
